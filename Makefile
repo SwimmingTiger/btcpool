be3788089e04859d2cd8c006abfcc6a4c44bc9ef
SwimmingTiger <hu60.cn@gmail.com>
2017-02-14 14:50:15 +0800

    可以创建和读取节点了

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 test : test.c
-	libtool --mode=link --tag=CC gcc /usr/local/lib/libzookeeper_st.la test.c -o test
+	libtool --mode=link --tag=CC gcc /usr/local/lib/libzookeeper_mt.la test.c -o test
+
+test_st : test.c
+	libtool --mode=link --tag=CC gcc /usr/local/lib/libzookeeper_st.la test.c -o test_st
+
+clean : 
+	rm -f test test_mt test_st
--- test.c
+++ test.c
@@ -10,8 +10,23 @@ void my_watcher(zhandle_t *zh, int type, int state, const char *path, void *watc
 
 int main() {
     zhandle_t *zh = NULL;
+    int stat = 0;
+    char path[255] = {0};
+    char buff[1024] = {0};
+    int len;
 
-    zh = zookeeper_init("127.0.0.1:9092", my_watcher, 10, NULL, NULL, 0);
+    zh = zookeeper_init("127.0.0.1:2181", my_watcher, 10000, NULL, NULL, 0);
+
+    printf("----------------- Create ----------------\n");
+    stat = zoo_create(zh, "/jobmaker/lock", "hello", 6, &ZOO_OPEN_ACL_UNSAFE, ZOO_EPHEMERAL | ZOO_SEQUENCE, path, 255);
+    printf("stat: %s -> %s\n", zerror(stat), path);
+
+    stat = zoo_get(zh, path, 0, buff, &len, NULL);
+    printf("stat: %s -> (%d) %s\n", zerror(stat), len, buff);
+
+    getchar();
+
+    zookeeper_close(zh);
 
     return 0;
 }
